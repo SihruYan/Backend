033b7905a7fdfd7e5fa1cae2591ea7d26bd554f0
Adrian <Adrian.Yan@itrust-insights.com>
2025-05-21 05:09:00 -0400

    test

---
+++ Model/Product.cs
@@ -0,0 +1,9 @@
+namespace Backend.Model;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string ImageUrl { get; set; } = "";
+}
